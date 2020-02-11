b34284069a37175f22557bc3af16d04507b895a6
sophie <a@a.fr>
2020-02-11 14:41:19 +0100

    V01
    Debut du memorie creation des deux tableaux

---
+++ main.js
@@ -0,0 +1,48 @@
+var tableau = [
+	{'ID':1, 'family':'magritte','src':'img/img01.jpg'},
+	{'ID':2, 'family':'picasso','src':'img/img02.jpg'},
+	{'ID':3, 'family':'josef buys','src':'img/img03.jpg'},
+	{'ID':4, 'family':'shigeru ban','src':'img/img04.jpg'},
+	{'ID':5, 'family':'camille claudel','src':'img/img05.jpg'},
+	{'ID':6, 'family':'niki de saint phalle ','src':'img/img06.jpg'},
+	{'ID':7, 'family':'kreuger','src':'img/img07.jpg'},
+	{'ID':8, 'family':'douanier','src':'img/img08.jpg'},
+	{'ID':9, 'family':'rodin','src':'img/img09.jpg'},
+	{'ID':10, 'family':'vasarely','src':'img/img10.jpg'},
+	{'ID':11, 'family':'lachapelle','src':'img/img11.jpg'},
+	{'ID':12, 'family':'malevitch','src':'img/img12.jpg'},
+	{'ID':13, 'family':'soulages','src':'img/img13.jpg'},
+	{'ID':14, 'family':'kapour','src':'img/img14.jpg'},
+	{'ID':15, 'family':'frida khalo','src':'img/img15.jpg'},
+	{'ID':16, 'family':'modigliani','src':'img/img16.jpg'},
+	{'ID':17, 'family':'van gogh','src':'img/img17.jpg'},
+	{'ID':18, 'family':'sagmeister','src':'img/img18.jpg'},
+	{'ID':19, 'family':'murakami','src':'img/img19.jpg'},
+	{'ID':20, 'family':'saracano','src':'img/img20.jpg'},
+	{'ID':21, 'family':'magritte','src':'img/img01.jpg'},
+	{'ID':22, 'family':'picasso','src':'img/img02.jpg'},
+	{'ID':23, 'family':'josef buys','src':'img/img03.jpg'},
+	{'ID':24, 'family':'shigeru ban','src':'img/img04.jpg'},
+	{'ID':25, 'family':'camille claudel','src':'img/img05.jpg'},
+	{'ID':26, 'family':'niki de saint phalle ','src':'img/img06.jpg'},
+	{'ID':27, 'family':'kreuger','src':'img/img07.jpg'},
+	{'ID':28, 'family':'douanier','src':'img/img08.jpg'},
+	{'ID':29, 'family':'rodin','src':'img/img09.jpg'},
+	{'ID':30, 'family':'vasarely','src':'img/img10.jpg'},
+	{'ID':31, 'family':'lachapelle','src':'img/img11.jpg'},
+	{'ID':32, 'family':'malevitch','src':'img/img12.jpg'},
+	{'ID':33, 'family':'soulages','src':'img/img13.jpg'},
+	{'ID':34, 'family':'kapour','src':'img/img14.jpg'},
+	{'ID':35, 'family':'frida khalo','src':'img/img15.jpg'},
+	{'ID':36, 'family':'modigliani','src':'img/img16.jpg'},
+	{'ID':37, 'family':'van gogh','src':'img/img17.jpg'},
+	{'ID':38, 'family':'sagmeister','src':'img/img18.jpg'},
+	{'ID':39, 'family':'murakami','src':'img/img19.jpg'},
+	{'ID':40, 'family':'saracano','src':'img/img20.jpg'},	
+
+];
+
+
+for (var i = 0; i < tableau.length; i++) {
+	
+}
